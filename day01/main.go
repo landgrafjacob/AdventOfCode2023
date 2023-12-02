38c2c1e13e7803f1995fa45457a343be89c1c748
Jake Landgraf <landgrafjacob@gmail.com>
2023-12-02 05:11:38 -0500

    Add day01, part 2

--- day01/main.go
+++ day01/main.go
@@ -21,53 +21,71 @@ var numMap = map[string]string{
 }
 
 func main() {
-	f, err := os.Open("input_test_2.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error opening input file")
 	}
 
 	scanner := bufio.NewScanner(f)
 
-	answer := 0
+	answer1 := 0
+	answer2 := 0
 
 	for scanner.Scan() {
-		lineValue := getCalValue(scanner.Text())
-		answer = answer + lineValue
-		fmt.Printf("%s: %d\n", scanner.Text(), lineValue)
+		lineValuePart1 := getCalValue(scanner.Text(), false)
+		lineValuePart2 := getCalValue(scanner.Text(), true)
+		answer1 = answer1 + lineValuePart1
+		answer2 = answer2 + lineValuePart2
+		// fmt.Printf("%s: %d\n", scanner.Text(), lineValue)
 	}
 
-	fmt.Printf("Calibration value: %d\n", answer)
+	fmt.Printf("Calibration value Part 1: %d\n", answer1)
+	fmt.Printf("Calibration value Part 2: %d\n", answer2)
 }
 
-func getFirstNum(line string) string {
+func getFirstNum(line string, includeSpelled bool) string {
 	for i, char := range line {
 		charString := string(char)
 		if regexp.MustCompile(`\d`).MatchString(charString) {
 			return charString
 		}
-		for j := 3; j <= 5; j++ {
-			if num, ok := numMap[line[i:i+3]]; ok {
-				return num
+		if includeSpelled {
+			for j := 3; j <= 5; j++ {
+				if i+j > len(line) {
+					continue
+				}
+				if num, ok := numMap[line[i:i+j]]; ok {
+					return num
+				}
 			}
 		}
 	}
 	return ""
 }
 
-func getLastNum(line string) string {
+func getLastNum(line string, includeSpelled bool) string {
 	for i := len(line) - 1; i >= 0; i-- {
 		charString := string(line[i])
 		if regexp.MustCompile(`\d`).MatchString(charString) {
 			return charString
 		}
+		if includeSpelled {
+			for j := 3; j <= 5; j++ {
+				if i+j > len(line) {
+					continue
+				}
+				if num, ok := numMap[line[i:i+j]]; ok {
+					return num
+				}
+			}
+		}
 	}
 	return ""
 }
 
-func getCalValue(line string) int {
-	first := getFirstNum(line)
-	second := getLastNum(line)
-	fmt.Printf("%s: %s", line, first)
+func getCalValue(line string, includeSpelled bool) int {
+	first := getFirstNum(line, includeSpelled)
+	second := getLastNum(line, includeSpelled)
 
 	result, _ := strconv.Atoi(first + second)
 	return result
